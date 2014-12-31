1bea3c43d8932f68279a28502d7715e9d42c7af7
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-30 21:41:37 -0800

    form input

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @input = Input.new
+  end
+
+  def new
+    @input = Input.new
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "/inputs/new",    to: "home#new",     as: :new_input
+  post "/inputs",       to: "home#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
