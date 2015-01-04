4ba0bfd6f42821677b701100c6e9dd77420dc1f0
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-03 22:01:15 -0800

    add another moving shape

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,12 +1,26 @@
 $(function(){
   loop();
+  secondLoop();
 });
 
 function loop() {
   $("#circle").animate({
     opacity: 0.25,
-    left: '+=20em',
-    height: '+=1em'}, 5000, 'linear',  function() {
+    left: "+=2em",
+    height: "+=1em",
+    width: "+=2em",
+    }, 5000, 'linear',  function() {
       loop();
+  });
+}
+
+function secondLoop() {
+  $("#second-circle").animate({
+    opacity: 0.3,
+    left: "+=2em",
+    height: "+=1em",
+    width: "+=5em",
+  }, 4000, 'linear',  function() {
+    loop();
   });
 }
